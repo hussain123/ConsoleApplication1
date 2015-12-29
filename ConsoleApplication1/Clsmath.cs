88d8f36f51a8d818e1a4b4c91d759a12916ecd91
hussain mustafa <hussain mustafa>
2015-12-29 14:00:28 +0500

    revision one

---
+++ ConsoleApplication1/Clsmath.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Clsmath
+    {
+
+        public delegate int pointerToadd(int x, int y);
+
+        public pointerToadd getPointer(int i) 
+        {
+            pointerToadd operation = null;
+
+            if (i == 1)
+            {
+                operation = Add;
+            }
+            if (i == 2)
+            {
+                operation = Sub;
+            }
+            if (i == 3)
+            {
+                operation = Sub;
+            }
+
+            return operation;
+        }
+        private int Add(int i, int y)
+        {
+            return i + y;
+        }
+        private int Sub(int i, int y)
+        {
+            return i - y;
+        }
+        private int Multi(int i, int y)
+        {
+            return i * y;
+        }
+        private int Div(int i, int y)
+        {
+            return i / y;
+        }
+
+    }
+
+    class program
+    {
+
+
+         static void Main(String[] args) {
+
+
+             Clsmath objMath = new Clsmath();
+             int[] input= new int[2];
+              input[0] = Convert.ToInt32(Console.ReadLine());
+              input[1] = Convert.ToInt32(Console.ReadLine());
+              int result = objMath.getPointer(1).Invoke(input[0], input[1]);
+             Console.WriteLine(result);
+             Console.ReadLine();
+         
+         }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate void StringProcessor (string input);
+namespace ConsoleApplication1
+{
+    class Person
+    {
+        string name;
+
+        public Person(string name)
+        {
+            this.name = name;
+
+        }
+        public void Say(String message)
+        {
+            Console.WriteLine("{0}, says: {1}", name, message);
+        }
+    }
+
+    class Background
+    {
+        public static void Note(string note)
+        {
+            Console.WriteLine("({0})", note);
+        }
+    }
+
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Person jon = new Person("jon");
+        //    Person tom = new Person("tom");
+
+        //    StringProcessor JonsVoice, tomsVoice, background;
+
+
+        //    JonsVoice = new StringProcessor(jon.Say);
+        //    tomsVoice = new StringProcessor(tom.Say);
+        //    background = new StringProcessor(Background.Note);
+
+
+        //    JonsVoice("hello son.");
+        //    tomsVoice.Invoke("hello daddy");
+        //    background("an airplane fliees past ");
+
+
+
+
+        //    Console.ReadLine();
+        //}
+    }
+
+
+
+}
